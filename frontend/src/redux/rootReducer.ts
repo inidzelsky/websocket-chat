a6ef844451cf469d345b731fd67fc5c0f30d230b
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-01-17 22:02:32 +0200

    Init Redux

---
+++ frontend/src/redux/rootReducer.ts
@@ -0,0 +1,13 @@
+// type InitialStateType = {}
+
+const initialState = {
+};
+
+const rootReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
