2bb313bb7988005d9ae3c62507e871a4092a2d9d
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-01-18 20:51:04 +0200

    Import pg and set connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { createServer } from 'http';
 import Koa from 'koa';
 import { Server, Socket } from 'socket.io';
+import { Pool } from 'pg';
 
 const app = new Koa();
 const server = createServer(app.callback());
@@ -9,6 +10,13 @@ const io = new Server(server, {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
+});
+
+const pool = new Pool({
+  user: 'obismarck',
+  host: '127.0.0.1',
+  database: 'websocketchat',
+  port: 5432,
 });
 
 io.on('connection', (socket: Socket) => {
